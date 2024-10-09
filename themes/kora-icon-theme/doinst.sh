cc7c7ab0c7531f0ca52160e0e8f67c8f4fa32cb3
lecramyajiv <vijaymarcel@gmail.com>
2024-10-09 16:40:07 +0530

    add kora

---
+++ themes/kora-icon-theme/doinst.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ -x /usr/bin/update-mime-database ]; then
+  /usr/bin/update-mime-database usr/share/mime >/dev/null 2>&1
+fi
+
+if [ -e usr/share/icons/kora/icon-theme.cache ]; then
+  if [ -x /usr/bin/gtk-update-icon-cache ]; then
+    /usr/bin/gtk-update-icon-cache -f usr/share/icons/kora >/dev/null 2>&1
+  fi
+fi
+
