356fd481f26386a978d332d80aa001cb84d0333a
Vijay Marcel <vijaymarcel@gmail.com>
2025-12-19 23:39:42 +0530

    rm

--- tde-packaging/packaging-trinity-14.1.3/redhat/core/tdebindings/filter-requires.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if [ -x /usr/lib/rpm/redhat/find-requires ] ; then
-FINDREQ=/usr/lib/rpm/redhat/find-requires
-else
-FINDREQ=/usr/lib/rpm/find-requires
-fi
-
-$FINDREQ $* | sed -e '/libcairo.so.2/d' -e '/libpangocairo-1.0.so.0/d'
